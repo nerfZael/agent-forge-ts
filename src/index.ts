c378e326a9861c3f1a4f846449c1c9cb970bd524
namesty <nestor09amesty@gmail.com>
2023-09-02 20:02:10 +0200

    Mock loop funcs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,46 @@
-import { Args_main, ModuleBase } from "./wrap";
+import { Args_main, Args_run, Args_runStep, ModuleBase, Step } from "./wrap";
+
+class State {
+  index = 0;
+  output: string | null = null;
+}
+
+const encode = (state: State): string => {
+  return JSON.stringify(state);
+}
+
+const decode = (stateString: string): State => {
+  return JSON.parse(stateString);
+}
 
 export class Module extends ModuleBase {
+  run(args: Args_run): Step {
+    const state = new State();
+
+    const result = {
+      state: encode(state),
+      output: `Agent says: I started with goal: ${args.goal}`
+    }
+
+    return result;
+  }
+  runStep(args: Args_runStep): Step {
+    const state = decode(args.state);
+
+    state.index++;
+
+    if (state.index > 5) {
+      return {
+        state: encode(state),
+        output: "Agent says: I finished"
+      }
+    }
+
+    return {
+      state: encode(state),
+      output: `Agent says: ${state.index} step`
+    };
+  }
   main(args: Args_main): number {
     throw new Error("Method not implemented.");
   }
