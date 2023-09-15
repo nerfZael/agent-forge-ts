e960fc3e90c5bd16e0a8eed3ef3ac27de816d363
nerfZael <bogunovij@gmail.com>
2023-09-15 18:34:19 +0200

    changed default port

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_PORT = 8080;
+export const DEFAULT_PORT = 8000;
 export const THIS_URI = "https/http.wrappers.dev/u/test/agent-forge-ts";
 export const THIS_URL = "https/http.wrappers.dev/u/test/agent-forge-ts";
 export const OPENAI_API_KEY = "";
